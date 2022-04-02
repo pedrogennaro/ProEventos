149eb23ea1573d427c4d073bd58c07828a7d9e61
Pedro <pedrogennaro@gmail.com>
2022-04-02 02:54:36 -0300

    Seção 7: Angular - Organizar, Rota, Alerta e +

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -14,13 +14,17 @@ import { NgxSpinnerModule } from "ngx-spinner";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import { NavComponent } from './nav/nav.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 import { EventoService } from './services/evento.service';
 
 import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
+import { TituloComponent } from './shared/titulo/titulo.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
 
 
@@ -30,6 +34,10 @@ import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
       EventosComponent,
       PalestrantesComponent,
       NavComponent,
+      TituloComponent,
+      ContatosComponent,
+      DashboardComponent,
+      PerfilComponent,
       DateTimeFormatPipe
    ],
   imports: [
