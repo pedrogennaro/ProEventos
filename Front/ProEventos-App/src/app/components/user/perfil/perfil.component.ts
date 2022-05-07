69834b8bac861fc3e24b0fe99a66fbbb88942f74
Pedro <pedrogennaro@gmail.com>
2022-05-07 00:00:26 -0300

    Seção 8: Reactive Forms e Mudança de Layout

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.validation();
+  }
+
+  private validation(): void{
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmarSenha')
+    }
+
+    this.form = this.fb.group({
+      titulo: ['', Validators.required],
+      primeiroNome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      ultimoNome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', Validators.required],
+      funcao: ['', Validators.required],
+      descricao: ['', Validators.required],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmarSenha: ['', Validators.required]
+    }, formOptions);
+
+  }
+
+  public resetForm(event: any): void{
+    event.preventDefault();
+    this.form.reset();
   }
 
 }
